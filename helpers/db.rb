22d43c460bbeac4774e560749d0c479582406bbf
Kevin Hanna <kevindhanna@live.com>
2020-12-31 18:14:35 +0000

    add reset, handle weird stuff

--- helpers/db.rb
+++ helpers/db.rb
@@ -2,7 +2,16 @@ require 'mysql2'
 
 class Db
   def initialize
-      @client = Mysql2::Client.new(:host => ENV["HOST"], :username => "root", database: "quarankobi")
+    @client = Mysql2::Client.new(:host => ENV['HOST'],
+                                 :username => ENV['USERNAME'],
+                                 :password => ENV["PASSWORD"],
+                                 :database => ENV['DATABASE'],
+                                 :reconnect => true)
+  end
+
+  def reset
+    @client.query("DELETE FROM visits")
+    @client.query("DELETE FROM days")
   end
 
   def visits(ip)
@@ -20,10 +29,29 @@ class Db
     @client.query("UPDATE visits SET count=count+1 WHERE ip='#{ip}'")
   end
 
+  def day(ip)
+    if !has_day(ip)
+      add_day(ip)
+    end
+
+    results = @client.query("SELECT day FROM days WHERE ip='#{ip}'")
+    results.each do |r|
+      return r['day'].to_i
+    end
+  end
+
+  def next_day(ip)
+    @client.query("UPDATE days SET day=day+1 WHERE ip = '#{ip}'")
+  end
+
   private
 
   def add_ip(ip)
     @client.query("INSERT INTO visits (ip, count) VALUES ('#{ip}', 0)")
+  end
+
+  def add_day(ip)
+    @client.query("INSERT INTO days (ip, day) VALUES ('#{ip}', 1)")
   end
 
   def has_visited(ip)
@@ -35,4 +63,12 @@ class Db
     false
   end
 
+  def has_day(ip)
+    result = @client.query("SELECT ip FROM days WHERE ip = '#{ip}'")
+    result.each do |r|
+      return true if r['ip'] == ip
+    end
+
+    false
+  end
 end
--- quarankobi.rb
+++ quarankobi.rb
@@ -12,20 +12,44 @@ class Base < Sinatra::Base
                    layout_options: {views: 'views/layouts'}
   end
 
+  get '/reset' do
+    DB.reset
+    "DONE"
+  end
+
+  get '/' do
+    day = DB.day(request.ip)
+    redirect "day_#{day}"
+  end
+
   get '/day_1' do
     haml :day_1
   end
 
   get '/day_2' do
+    day = DB.day(request.ip)
+    if day < 2
+      redirect '/'
+    end
+
     score = params['score'].to_i || nil
     haml :day_2, locals: {score: score}
   end
 
   get '/day_3' do
+    day = DB.day(request.ip)
+    if day < 3
+      redirect '/'
+    end
+
     DB.visit(request.ip)
     count = DB.visits(request.ip)
-
     message = "-.-./-.../...-/.-/--./-.--/.-./..-./..-.!".split("")
     haml :day_3, locals: {count: count, message: message}
+  end
+
+  get '/next_day' do
+    DB.next_day(request.ip)
+    redirect '/'
   end
 end
