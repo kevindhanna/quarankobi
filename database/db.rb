c83501e99250e4321a35d97a15560ffea3b9b7b4
Kevin Hanna <kevindhanna@live.com>
2021-01-04 11:16:40 +0000

    "fix" day 3 refresh throws

--- database/db.rb
+++ database/db.rb
@@ -7,8 +7,20 @@ class Db
     @client = setup_db
   end
 
+  # gross wrapper to stop throws for very quick refreshes on day 3
+  def query(sql, sleep_count = 0)
+    begin
+      @client.query(sql)
+    rescue Mysql2::Error => e
+      # throw if we've been waiting for 2 seconds
+      throw e if sleep_count > 48
+      sleep(1.0/24.0)
+      query(sql, sleep_count + 1)
+    end
+  end
+
   def day_data
-    results = @client.query("SELECT * from peeps")
+    results = query("SELECT * from peeps")
     results = results.map do |entry|
       {
         ip: entry['ip'],
@@ -22,19 +34,19 @@ class Db
   end
 
   def reset
-    @client.query("DELETE FROM visits")
-    @client.query("DELETE FROM peeps")
-    @client.query("DELETE FROM day_6_answers")
-    @client.query("DELETE FROM day_11_history")
-    @client.query("DELETE FROM day_9")
+    query("DELETE FROM visits")
+    query("DELETE FROM peeps")
+    query("DELETE FROM day_6_answers")
+    query("DELETE FROM day_11_history")
+    query("DELETE FROM day_9")
   end
 
   def cheatering(ip, day, completed)
-    @client.query("UPDATE peeps SET day=#{day}, completed=#{completed}, reached='#{DateTime.now}' WHERE ip='#{ip}'")
+    query("UPDATE peeps SET day=#{day}, completed=#{completed}, reached='#{DateTime.now}' WHERE ip='#{ip}'")
   end
 
   def visits(ip)
-    results = @client.query("SELECT count FROM visits WHERE ip='#{ip}'")
+    results = query("SELECT count FROM visits WHERE ip='#{ip}'")
     results.each do |r|
       return r['count'].to_i
     end
@@ -42,9 +54,9 @@ class Db
 
   def visit(ip)
     if !visited?(ip)
-      @client.query("INSERT INTO visits (ip, count) VALUES ('#{ip}', 0)")
+      query("INSERT INTO visits (ip, count) VALUES ('#{ip}', 0)")
     end
-    @client.query("UPDATE visits SET count=count+1 WHERE ip='#{ip}'")
+    query("UPDATE visits SET count=count+1 WHERE ip='#{ip}'")
   end
 
   def day(ip)
@@ -52,7 +64,7 @@ class Db
       add_ip(ip)
     end
 
-    results = @client.query("SELECT day, reached, completed, name FROM peeps WHERE ip='#{ip}'")
+    results = query("SELECT day, reached, completed, name FROM peeps WHERE ip='#{ip}'")
     results.each do |r|
       return [r['day'], r['reached'], (r['completed'] == 1), r['name']]
     end
@@ -60,27 +72,27 @@ class Db
 
   def next_day(ip)
     date = DateTime.now
-    @client.query("UPDATE peeps SET day=day+1, completed=false, reached='#{date}' WHERE ip = '#{ip}'")
+    query("UPDATE peeps SET day=day+1, completed=false, reached='#{date}' WHERE ip = '#{ip}'")
   end
 
   def complete(ip, day)
-    @client.query("UPDATE peeps SET completed=true WHERE ip='#{ip}' AND day=#{day}")
+    query("UPDATE peeps SET completed=true WHERE ip='#{ip}' AND day=#{day}")
   end
 
   def set_name(ip, name)
-    @client.query("UPDATE peeps SET name='#{name}' WHERE ip='#{ip}'")
+    query("UPDATE peeps SET name='#{name}' WHERE ip='#{ip}'")
   end
 
   def set_day_6_answers(ip, answers)
     if !answered?(ip)
-      @client.query("INSERT INTO day_6_answers (ip, answers) VALUES ('#{ip}', '#{answers}')")
+      query("INSERT INTO day_6_answers (ip, answers) VALUES ('#{ip}', '#{answers}')")
     else
-      @client.query("UPDATE day_6_answers SET answers='#{answers}' where ip='#{ip}'")
+      query("UPDATE day_6_answers SET answers='#{answers}' where ip='#{ip}'")
     end
   end
 
   def day_6_answers(ip)
-    result = @client.query("SELECT answers FROM day_6_answers WHERE ip='#{ip}'")
+    result = query("SELECT answers FROM day_6_answers WHERE ip='#{ip}'")
     result.each do |r|
       return r['answers']
     end
@@ -89,13 +101,13 @@ class Db
 
   def set_day_11_history(ip, history)
     if !has_history(ip)
-      @client.query("INSERT INTO day_11_history (ip, history) VALUES ('#{ip}', '#{history}')")
+      query("INSERT INTO day_11_history (ip, history) VALUES ('#{ip}', '#{history}')")
     end
-    @client.query("UPDATE day_11_history SET history='#{history}' WHERE ip='#{ip}'")
+    query("UPDATE day_11_history SET history='#{history}' WHERE ip='#{ip}'")
   end
 
   def day_11_history(ip)
-    results =  @client.query("SELECT history FROM day_11_history WHERE ip='#{ip}'")
+    results =  query("SELECT history FROM day_11_history WHERE ip='#{ip}'")
     results.each do |r|
       return r['history']
     end
@@ -103,23 +115,23 @@ class Db
   end
 
   def name(ip)
-    result = @client.query("SELECT name FROM peeps WHERE ip='#{ip}'")
+    result = query("SELECT name FROM peeps WHERE ip='#{ip}'")
     result.each do |r|
       return r['name']
     end
   end
 
   def set_day_9(ip, twister)
-      @client.query("UPDATE day_9 SET twister=#{twister} where ip='#{ip}'")
+      query("UPDATE day_9 SET twister=#{twister} where ip='#{ip}'")
   end
 
   def day_9(ip)
     if !has_day_9?(ip)
-      @client.query("INSERT INTO day_9 (ip, twister) VALUES ('#{ip}', 0)")
+      query("INSERT INTO day_9 (ip, twister) VALUES ('#{ip}', 0)")
       return 1
     end
 
-    result = @client.query("SELECT twister FROM day_9 where ip='#{ip}'")
+    result = query("SELECT twister FROM day_9 where ip='#{ip}'")
     result.each do |r|
       return r['twister']
     end
@@ -129,11 +141,11 @@ class Db
 
   def add_ip(ip)
     date = DateTime.now
-    @client.query("INSERT INTO peeps (ip, day, completed, reached) VALUES ('#{ip}', 1, false, '#{date}')")
+    query("INSERT INTO peeps (ip, day, completed, reached) VALUES ('#{ip}', 1, false, '#{date}')")
   end
 
   def played?(ip)
-    result = @client.query("SELECT ip FROM peeps WHERE ip = '#{ip}'")
+    result = query("SELECT ip FROM peeps WHERE ip = '#{ip}'")
     result.each do |r|
       return true if r['ip'] == ip
     end
@@ -142,7 +154,7 @@ class Db
   end
 
   def visited?(ip)
-    result = @client.query("SELECT ip FROM visits WHERE ip = '#{ip}'")
+    result = query("SELECT ip FROM visits WHERE ip = '#{ip}'")
     result.each do |r|
       return true if r['ip'] == ip
     end
@@ -151,7 +163,7 @@ class Db
   end
 
   def answered?(ip)
-    result = @client.query("SELECT ip FROM day_6_answers WHERE ip = '#{ip}'")
+    result = query("SELECT ip FROM day_6_answers WHERE ip = '#{ip}'")
     result.each do |r|
       return true if r['ip'] == ip
     end
@@ -160,7 +172,7 @@ class Db
   end
 
   def has_history(ip)
-    result = @client.query("SELECT ip FROM day_11_history WHERE ip = '#{ip}'")
+    result = query("SELECT ip FROM day_11_history WHERE ip = '#{ip}'")
     result.each do |r|
       return true if r['ip'] == ip
     end
@@ -169,7 +181,7 @@ class Db
   end
 
   def has_day_9?(ip)
-    result = @client.query("SELECT ip FROM day_9 WHERE ip = '#{ip}'")
+    result = query("SELECT ip FROM day_9 WHERE ip = '#{ip}'")
     result.each do |r|
       return true if r['ip'] == ip
     end
