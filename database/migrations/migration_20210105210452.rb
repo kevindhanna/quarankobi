39b1ed3f86bb30a4381fd3b0cd5cd932aad4f094
Kevin Hanna <kevindhanna@live.com>
2021-01-05 22:46:50 +0000

    stringify migrations

--- database/migrations/migration_20210105210452.rb
+++ database/migrations/migration_20210105210452.rb
@@ -1,6 +1,6 @@
 def run(client)
   client.query("ALTER TABLE peeps ADD COLUMN (day_6_answers varchar(255))")
   client.query("SELECT id, answers FROM day_6_answers").each do |v|
-    client.query("UPDATE peeps SET day_6_answers = #{v["answers"]} WHERE id='#{v["id"]}'")
+    client.query("UPDATE peeps SET day_6_answers = '#{v["answers"]}' WHERE id='#{v["id"]}'")
   end
 end
